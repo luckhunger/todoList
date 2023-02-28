110c949111b0c4f0b6aa1522317420e8ce1ae73b
DESKTOP-1CKD89I\admin <dmjku16@gmail.com>
2023-02-28 11:24:45 +0900

    서버 및 화면 띄우기

---
+++ src/main/java/com/example/todoList/controller/ListController.java
@@ -0,0 +1,15 @@
+package com.example.todoList.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ListController {
+    @GetMapping("list")
+    public String list(Model model) {
+        // model : MVC 중 model
+        model.addAttribute("data", "hello!!");
+        return "list";
+    }
+}
